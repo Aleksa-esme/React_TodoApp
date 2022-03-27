6fbe6058da04f5efa23ca65b6f4d1b8f4c70a687
Aleksa-esme <esme1990@mail.ru>
2022-03-27 19:34:46 +0300

    access control added

--- src/TodoDetail.js
+++ src/TodoDetail.js
@@ -1,18 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 
 export default function TodoDetail(props) {
     const {key} = useParams();
     const deed = props.getDeed(key);
-
-    return (
-        <section>
-            {deed.done && 
-                <p className="has-text-success">Выполнено</p>
-            }
-            <h1>{deed.title}</h1>
-            <p>{deed.createdAt.toLocaleString()}</p>
-            {deed.desc && <p>{deed.desc}</p>}
-            {deed.image && <p><img src={deed.image} alt='Иллюстрация' /></p>}
-        </section>
-    );
+    if (!props.currentUser)
+        return <Navigate to='/login' replace />;
+    else
+        return (
+            <section>
+                {deed.done && 
+                    <p className="has-text-success">Выполнено</p>
+                }
+                <h1>{deed.title}</h1>
+                <p>{deed.createdAt.toLocaleString()}</p>
+                {deed.desc && <p>{deed.desc}</p>}
+                {deed.image && <p><img src={deed.image} alt='Иллюстрация' /></p>}
+            </section>
+        );
 }
